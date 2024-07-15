e273202ad5fe730729333eb77cff437ba1fc243e
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-07-15 18:52:10 +0300

    Add files via upload

---
+++ 07.StringExplosion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] divided = input.Split('>');
+            int explosionStrength = 0;
+            int remainingExplosionStrength = 0;
+            for (int i = 1; i < divided.Length; i++)
+            {
+                explosionStrength = int.Parse("" + divided[i][0]) + remainingExplosionStrength;
+                remainingExplosionStrength = explosionStrength - divided[i].Length;
+                if (explosionStrength > divided[i].Length)
+                {
+                    explosionStrength = divided[i].Length;
+                }
+                    divided[i] = divided[i].Substring(explosionStrength);
+                if (remainingExplosionStrength < 0)
+                {
+                    remainingExplosionStrength = 0;
+                }
+            }
+            string result = string.Join(">", divided);
+            Console.WriteLine(result);
+        }
+    }
+}
