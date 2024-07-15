13416aff750d70736f59f659d7bfb2ab35c27d3d
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-07-15 18:51:03 +0300

    Add files via upload

---
+++ 06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string input = Console.ReadLine();
+                string result = ReplaceRepeatingChars(input);
+                Console.WriteLine(result);
+        }
+            public static string ReplaceRepeatingChars(string str)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return string.Empty;
+                }
+                char currentChar = str[0];
+                string result = currentChar.ToString();
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (str[i] != currentChar)
+                    {
+                        currentChar = str[i];
+                        result += currentChar;
+                    }
+                }
+                return result;
+            }
+        }
+    }
